0359d277b953d6f4d57a3d126a5e5fdcd8e01be4
mle <caomaxou@gmail.com>
2023-05-20 21:16:28 +0200

    H2_RHF_STO3G.py

--- examples/abinit/H2_RHF_STO3G.py
+++ examples/abinit/H2_RHF_STO3G.py
@@ -15,7 +15,7 @@ pi = np.pi
 def overlap_int(molec):
     n = molec.n
     S = np.zeros([n,n])
-    
+
     # double loops over the atoms
     for i in range(n):
         atomi = molec.atoms[i]
@@ -32,7 +32,7 @@ def overlap_int(molec):
             # double loops over basis functions
             for k in range(ni):
                 basis_ik = basisi[k]
-        
+
                 for l in range(nj):
                     basis_jl = basisj[l]
 
@@ -44,7 +44,7 @@ def T_int(molec):
 
     n = molec.n
     T = np.zeros([n,n])
-    
+
     # double loops over the atoms
     for i in range(n):
         atomi = molec.atoms[i]
@@ -61,7 +61,7 @@ def T_int(molec):
             # double loops over basis functions
             for k in range(ni):
                 basis_ik = basisi[k]
-        
+
                 for l in range(nj):
                     basis_jl = basisj[l]
 
@@ -73,7 +73,7 @@ def Vep_int(molec):
 
     n = molec.n
     Vep = np.zeros([n,n])
-    
+
     for atom in molec.atoms:
         Xp = atom.x
         Z  = atom.Z
@@ -96,7 +96,7 @@ def Vep_int(molec):
                     for l in range(nj):
                         b2 = basisj[l]
 
-                        Vep[i,j] -= Z*coeffi[k]*coeffj[l]*PrimitiveGaussian.electron_proton_int(b1,b2,Xp) 
+                        Vep[i,j] -= Z*coeffi[k]*coeffj[l]*PrimitiveGaussian.electron_proton_int(b1,b2,Xp)
     return Vep
 
 def Vee_int(molec):
@@ -149,15 +149,15 @@ def Vee_int(molec):
                                     b4 = basisl[t]
 
                                     c_uvwt = c_u*c_v*c_w*c_t
-                                    vee_ijkl = c_uvwt * PrimitiveGaussian.electron_electron_int(b1,b2,b3,b4) 
-                                    Vee[i,j,k,l] += vee_ijkl 
+                                    vee_ijkl = c_uvwt * PrimitiveGaussian.electron_electron_int(b1,b2,b3,b4)
+                                    Vee[i,j,k,l] += vee_ijkl
 
     return Vee
 
 def Vpp_int(molec):
     n = molec.n
     Vpp = 0.0
-    
+
     for i in range(n):
         atomi = molec.atoms[i]
         Zi = atomi.Z
@@ -166,7 +166,7 @@ def Vpp_int(molec):
             atomj = molec.atoms[j]
             Zj = atomj.Z
             xj = atomj.x
-            
+
             xij = xi - xj
             rij2 = np.inner(xij,xij)
             Vpp += Zi*Zj/m.sqrt(rij2)
@@ -177,7 +177,8 @@ def Vpp_int(molec):
 def G_int(molec,P):
     """computing two electron operators"""
     n = molec.n
-    G = np.zeros([n,n])
+    Vcoulomb = np.zeros([n,n])
+    Vx = np.zeros([n,n])
 
     # computing coulomb integrals
     Vee = Vee_int(molec)
@@ -190,18 +191,23 @@ def G_int(molec,P):
                     J = Vee[i,j,k,l]
                     K = Vee[i,j,l,k]
 
-                    G[i,j] += P[k,l]*(J-0.5*K)
-    return G
+                    #G[i,j] += P[k,l]*(J-0.5*K)
+                    Vcoulomb[i,j] += density*J
+                    Vx[i,j]       += density*(-0.5*K)
+    return Vcoulomb, Vx
 
 
 def P_mat(c,n):
     c = c[:,0:1]
-    P = 2.0 * c@c.T   
+    P = 2.0 * c@c.T
     return P
 
-def compute_Eelec(P,Hcore,G,n):
-    Eelec = np.sum(P*(Hcore+0.5*G))
-    return Eelec
+def compute_Eelec(P,Hcore,Vcoulomb,Vx,n):
+    #Eelec = np.sum(P*(Hcore+0.5*G))
+    Ecore = np.sum(P*Hcore)
+    Ecoulomb = np.sum(P*Vcoulomb)*0.5
+    Ex = np.sum(P*Vx)*0.5
+    return Ecore, Ecoulomb, Ex
 
 # scf loop
 def scf_loop(molec,Nmax=200,eps=1e-8):
@@ -211,7 +217,7 @@ def scf_loop(molec,Nmax=200,eps=1e-8):
 
     # compute overlap matrix
     S = overlap_int(molec)
-    S12  = linalg.sqrtm(S) 
+    S12  = linalg.sqrtm(S)
     S_12 = linalg.inv(S12)
 
     # integrals
@@ -224,15 +230,19 @@ def scf_loop(molec,Nmax=200,eps=1e-8):
         E_old = E
 
         # compute Fock operator
-        G   = G_int(molec,P) 
+        Vcoulomb, Vx   = G_int(molec,P)
+        G = Vcoulomb + Vx
         F = Hcore + G
+
         # solve secular equation
         Fu = np.dot(S_12,np.dot(F,S_12))
         eigval , eigvec = linalg.eigh(Fu)
         c = np.dot(S_12,eigvec)
 
         P = P_mat(c,n)
-        E = compute_Eelec(P,Hcore,G,n) 
+        Ecore, Ecoulomb, Ex = compute_Eelec(P,Hcore,Vcoulomb,Vx,n)
+
+        E = Ecore + Ecoulomb + Ex
 
         if abs(E-E_old)<eps:
             print("Converged !")
@@ -246,27 +256,30 @@ alpha1 = 0.3425250914E+01
 alpha2 = 0.6239137298E+00
 alpha3 = 0.1688554040E+00
 
-c1 = 0.1543289673E+00 
+c1 = 0.1543289673E+00
 c2 = 0.5353281423E+00
 c3 = 0.4446345422E+00
 coeff = (c1, c2, c3)
+
 n = 100
 dlin = np.linspace(0.4,20,n)
 
 Etot = []
-
-for d in dlin:
+Ecoulomb_list = []
+for i,d in enumerate(dlin):
 
     x1 = np.array([0.0, 0.0, 0.0])
     x2 = np.array([0.0, 0.0, d  ])
-    
+
+    Z = 1
+
     PG11 = PrimitiveGaussian(alpha1,x0=x1)
-    PG21 = PrimitiveGaussian(alpha2,x0=x1) 
-    PG31 = PrimitiveGaussian(alpha3,x0=x1)  
+    PG21 = PrimitiveGaussian(alpha2,x0=x1)
+    PG31 = PrimitiveGaussian(alpha3,x0=x1)
 
     PG12 = PrimitiveGaussian(alpha1,x0=x2)
-    PG22 = PrimitiveGaussian(alpha2,x0=x2) 
-    PG32 = PrimitiveGaussian(alpha3,x0=x2)  
+    PG22 = PrimitiveGaussian(alpha2,x0=x2)
+    PG32 = PrimitiveGaussian(alpha3,x0=x2)
 
     basis1 = (PG11, PG21, PG31)
     basis2 = (PG12, PG22, PG32)
@@ -274,19 +287,20 @@ for d in dlin:
     orbit1 = Orbital(coeff,basis1)
     orbit2 = Orbital(coeff,basis2)
 
-    Z = 1
-    H_1 = Atom(Z,x1,orbit1) 
+    H_1 = Atom(Z,x1,orbit1)
     H_2 = Atom(Z,x2,orbit2)
-    
+
     HH = (H_1, H_2)
-    
+
     molec = Molecule(HH)
     Eelec = scf_loop(molec)
-    Vpp = Vpp_int(molec) 
+    Vpp = Vpp_int(molec)
     Etot.append(Eelec+Vpp)
+
+    print(f'Opti {i} Done!')
 
 # output results
 import matplotlib.pyplot as plt
 
-plt.plot(dlin,Etot)
-plt.savefig("E.png")
+plt.plot(dlin,Etot,marker='x')
+plt.savefig("E2.png")
